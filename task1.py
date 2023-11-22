cd88f6cbafb503e72d43d69c1d9b2dbb6c5c26c6
高鑫杰 <2100013114@stu.pku.edu.cn>
2023-11-22 14:22:35 +0800

    1card

---
+++ task1.py
@@ -0,0 +1,65 @@
+import torchvision.transforms as transforms
+import cfg
+import utils
+from evaluate import validation_sam
+from utils import *
+
+args = cfg.parse_args()
+GPUdevice = torch.device('cuda', args.gpu_device)
+
+net = get_network(args, args.net, use_gpu=args.gpu, gpu_device = GPUdevice, distribution = args.distributed)
+
+'''load pretrained model'''
+if args.weights != 0:
+    print(f'=> resuming from {args.weights}')
+    assert os.path.exists(args.weights)
+    checkpoint_file = os.path.join(args.weights)
+    assert os.path.exists(checkpoint_file)
+    loc = 'cuda:{}'.format(args.gpu_device)
+    checkpoint = torch.load(checkpoint_file, map_location=loc)
+    start_epoch = checkpoint['epoch']
+    best_tol = checkpoint['best_tol']
+    
+    net.load_state_dict(checkpoint['state_dict'],strict=False)
+    # optimizer.load_state_dict(checkpoint['optimizer'], strict=False)
+
+    args.path_helper = checkpoint['path_helper']
+    logger = create_logger(args.path_helper['log_path'])
+    print(f'=> loaded checkpoint {checkpoint_file} (epoch {start_epoch})')
+
+args.path_helper = set_log_dir('logs', args.exp_name)
+logger = create_logger(args.path_helper['log_path'])
+logger.info(args)
+
+
+'''segmentation data'''
+transform_train = transforms.Compose([
+    transforms.Resize((args.image_size,args.image_size)),
+    transforms.ToTensor(),
+])
+
+transform_train_seg = transforms.Compose([
+    transforms.Resize((args.out_size,args.out_size)),
+    transforms.ToTensor(),
+])
+
+transform_test = transforms.Compose([
+    transforms.Resize((args.image_size, args.image_size)),
+    transforms.ToTensor(),
+])
+
+transform_test_seg = transforms.Compose([
+    transforms.Resize((args.out_size,args.out_size)),
+    transforms.ToTensor(),
+])
+
+nice_train_loader, nice_test_loader, transform_train, transform_val, train_list, val_list = get_decath_loader(args)
+
+'''begain valuation'''
+best_acc = 0.0
+best_tol = 1e4
+
+epoch = 1
+net.eval()
+tol, (eiou, edice) = validation_sam(args, nice_test_loader, epoch, net)
+logger.info(f'Total score: {tol}, IOU: {eiou}, DICE: {edice} || @ epoch {epoch}.')
---
+++ utils.py
@@ -0,0 +1,425 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Function
+import torchvision
+import torchvision.utils as vutils
+import os
+import sys
+import cfg
+import time
+import random
+import logging
+import numpy as np
+from datetime import datetime
+import dateutil.tz
+from monai.transforms import (
+    Compose,
+    CropForegroundd,
+    LoadImaged,
+    Orientationd,
+    RandFlipd,
+    RandCropByPosNegLabeld,
+    RandShiftIntensityd,
+    ScaleIntensityRanged,
+    Spacingd,
+    RandRotate90d,
+    EnsureTyped,
+)
+from monai.data import (
+    ThreadDataLoader,
+    CacheDataset,
+    load_decathlon_datalist,
+    set_track_meta,
+)
+
+args = cfg.parse_args()
+device = torch.device('cuda', args.gpu_device)
+
+class GpuDataParallel(object):
+    def __init__(self):
+        self.gpu_list = []
+        self.output_device = None
+
+    def set_device(self, device):
+        device = str(device)
+        if device != 'None':
+            self.gpu_list = [i for i in range(len(device.split(',')))]
+            os.environ["CUDA_VISIBLE_DEVICES"] = device
+            output_device = self.gpu_list[0]
+            self.occupy_gpu(self.gpu_list)
+        self.output_device = output_device if len(self.gpu_list) > 0 else "cpu"
+
+    def model_to_device(self, model):
+        # model = convert_model(model)
+        model = model.to(self.output_device)
+        if len(self.gpu_list) > 1:
+            model = nn.DataParallel(
+                model,
+                device_ids=self.gpu_list,
+                output_device=self.output_device)
+        return model
+
+    def data_to_device(self, data):
+        if isinstance(data, torch.FloatTensor):
+            return data.to(self.output_device)
+        elif isinstance(data, torch.DoubleTensor):
+            return data.float().to(self.output_device)
+        elif isinstance(data, torch.ByteTensor):
+            return data.long().to(self.output_device)
+        elif isinstance(data, torch.LongTensor):
+            return data.to(self.output_device)
+        elif isinstance(data, list) or isinstance(data, tuple):
+            return [self.data_to_device(d) for d in data]
+        else:
+            raise ValueError(data.shape, "Unknown Dtype: {}".format(data.dtype))
+
+    def criterion_to_device(self, loss):
+        return loss.to(self.output_device)
+
+    def occupy_gpu(self, gpus=None):
+        """
+            make program appear on nvidia-smi.
+        """
+        if len(gpus) == 0:
+            torch.zeros(1).cuda()
+        else:
+            gpus = [gpus] if isinstance(gpus, int) else list(gpus)
+            for g in gpus:
+                torch.zeros(1).cuda(g)
+
+def get_network(args, net, use_gpu=True, gpu_device = 0, distribution = True):
+    """ return given network
+    """
+
+    if net == 'sam':
+        from segment_anything import SamPredictor, sam_model_registry
+        from segment_anything.utils.transforms import ResizeLongestSide
+
+        net = sam_model_registry['vit_b'](checkpoint=args.sam_ckpt).to(device)
+    else:
+        print('the network name you have entered is not supported yet')
+        sys.exit()
+
+    if use_gpu:
+        #net = net.cuda(device = gpu_device)
+        if distribution != 'none':
+            net = torch.nn.DataParallel(net,device_ids=[int(id) for id in args.distributed.split(',')])
+            net = net.to(device=gpu_device)
+        else:
+            net = net.to(device=gpu_device)
+
+    return net
+
+def get_decath_loader(args):
+    train_transforms = Compose(
+        [   
+            LoadImaged(keys=["image", "label"], ensure_channel_first=True),
+            ScaleIntensityRanged(
+                keys=["image"],
+                a_min=-175,
+                a_max=250,
+                b_min=0.0,
+                b_max=1.0,
+                clip=True,
+            ),
+            CropForegroundd(keys=["image", "label"], source_key="image"),
+            Orientationd(keys=["image", "label"], axcodes="RAS"),
+            Spacingd(
+                keys=["image", "label"],
+                pixdim=(1.5, 1.5, 2.0),
+                mode=("bilinear", "nearest"),
+            ),
+            EnsureTyped(keys=["image", "label"], device=device, track_meta=False),
+            RandCropByPosNegLabeld(
+                keys=["image", "label"],
+                label_key="label",
+                spatial_size=(args.roi_size, args.roi_size, args.chunk),
+                pos=1,
+                neg=1,
+                num_samples=args.num_sample,
+                image_key="image",
+                image_threshold=0,
+            ),
+            RandFlipd(
+                keys=["image", "label"],
+                spatial_axis=[0],
+                prob=0.10,
+            ),
+            RandFlipd(
+                keys=["image", "label"],
+                spatial_axis=[1],
+                prob=0.10,
+            ),
+            RandFlipd(
+                keys=["image", "label"],
+                spatial_axis=[2],
+                prob=0.10,
+            ),
+            RandRotate90d(
+                keys=["image", "label"],
+                prob=0.10,
+                max_k=3,
+            ),
+            RandShiftIntensityd(
+                keys=["image"],
+                offsets=0.10,
+                prob=0.50,
+            ),
+        ]
+    )
+    val_transforms = Compose(
+        [
+            LoadImaged(keys=["image", "label"], ensure_channel_first=True),
+            ScaleIntensityRanged(
+                keys=["image"], a_min=-175, a_max=250, b_min=0.0, b_max=1.0, clip=True
+            ),
+            CropForegroundd(keys=["image", "label"], source_key="image"),
+            Orientationd(keys=["image", "label"], axcodes="RAS"),
+            Spacingd(
+                keys=["image", "label"],
+                pixdim=(1.5, 1.5, 2.0),
+                mode=("bilinear", "nearest"),
+            ),
+            EnsureTyped(keys=["image", "label"], device=device, track_meta=True),
+        ]
+    )
+
+    data_dir = args.data_path
+    split_JSON = "dataset_0.json"
+
+    datasets = os.path.join(data_dir, split_JSON)
+    datalist = load_decathlon_datalist(datasets, True, "training")
+    val_files = load_decathlon_datalist(datasets, True, "validation")
+    train_ds = CacheDataset(
+        data=datalist,
+        transform=train_transforms,
+        cache_num=24,
+        cache_rate=1.0,
+        num_workers=8,
+    )
+    train_loader = ThreadDataLoader(train_ds, num_workers=0, batch_size=args.b, shuffle=True)
+    val_ds = CacheDataset(
+        data=val_files, transform=val_transforms, cache_num=2, cache_rate=1.0, num_workers=0
+    )
+    val_loader = ThreadDataLoader(val_ds, num_workers=0, batch_size=1)
+
+    set_track_meta(False)
+
+    return train_loader, val_loader, train_transforms, val_transforms, datalist, val_files
+
+def create_logger(log_dir, phase='train'):
+    time_str = time.strftime('%Y-%m-%d-%H-%M')
+    log_file = '{}_{}.log'.format(time_str, phase)
+    final_log_file = os.path.join(log_dir, log_file)
+    head = '%(asctime)-15s %(message)s'
+    logging.basicConfig(filename=str(final_log_file),
+                        format=head)
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    console = logging.StreamHandler()
+    logging.getLogger('').addHandler(console)
+
+    return logger
+
+def set_log_dir(root_dir, exp_name):
+    path_dict = {}
+    os.makedirs(root_dir, exist_ok=True)
+
+    # set log path
+    exp_path = os.path.join(root_dir, exp_name)
+    now = datetime.now(dateutil.tz.tzlocal())
+    timestamp = now.strftime('%Y_%m_%d_%H_%M_%S')
+    prefix = exp_path + '_' + timestamp
+    os.makedirs(prefix)
+    path_dict['prefix'] = prefix
+
+    # set checkpoint path
+    ckpt_path = os.path.join(prefix, 'Model')
+    os.makedirs(ckpt_path)
+    path_dict['ckpt_path'] = ckpt_path
+
+    log_path = os.path.join(prefix, 'Log')
+    os.makedirs(log_path)
+    path_dict['log_path'] = log_path
+
+    # set sample image path for fid calculation
+    sample_path = os.path.join(prefix, 'Samples')
+    os.makedirs(sample_path)
+    path_dict['sample_path'] = sample_path
+
+    return path_dict
+
+def iou(outputs: np.array, labels: np.array):
+    
+    SMOOTH = 1e-6
+    intersection = (outputs & labels).sum((1, 2))
+    union = (outputs | labels).sum((1, 2))
+
+    iou = (intersection + SMOOTH) / (union + SMOOTH)
+
+    return iou.mean()
+
+class DiceCoeff(Function):
+    """Dice coeff for individual examples"""
+
+    def forward(self, input, target):
+        self.save_for_backward(input, target)
+        eps = 0.0001
+        self.inter = torch.dot(input.view(-1), target.view(-1))
+        self.union = torch.sum(input) + torch.sum(target) + eps
+
+        t = (2 * self.inter.float() + eps) / self.union.float()
+        return t
+
+    # This function has only a single output, so it gets only one gradient
+    def backward(self, grad_output):
+
+        input, target = self.saved_variables
+        grad_input = grad_target = None
+
+        if self.needs_input_grad[0]:
+            grad_input = grad_output * 2 * (target * self.union - self.inter) \
+                         / (self.union * self.union)
+        if self.needs_input_grad[1]:
+            grad_target = None
+
+        return grad_input, grad_target
+
+
+def dice_coeff(input, target):
+    """Dice coeff for batches"""
+    if input.is_cuda:
+        s = torch.FloatTensor(1).to(device = input.device).zero_()
+    else:
+        s = torch.FloatTensor(1).zero_()
+
+    for i, c in enumerate(zip(input, target)):
+        s = s + DiceCoeff().forward(c[0], c[1])
+
+    return s / (i + 1)
+
+def vis_image(imgs, pred_masks, gt_masks, save_path, reverse = False, points = None):
+    
+    b,c,h,w = pred_masks.size()
+    dev = pred_masks.get_device()
+    row_num = min(b, 4)
+
+    if torch.max(pred_masks) > 1 or torch.min(pred_masks) < 0:
+        pred_masks = torch.sigmoid(pred_masks)
+
+    if reverse == True:
+        pred_masks = 1 - pred_masks
+        gt_masks = 1 - gt_masks
+    if c == 2:
+        pred_disc, pred_cup = pred_masks[:,0,:,:].unsqueeze(1).expand(b,3,h,w), pred_masks[:,1,:,:].unsqueeze(1).expand(b,3,h,w)
+        gt_disc, gt_cup = gt_masks[:,0,:,:].unsqueeze(1).expand(b,3,h,w), gt_masks[:,1,:,:].unsqueeze(1).expand(b,3,h,w)
+        tup = (imgs[:row_num,:,:,:],pred_disc[:row_num,:,:,:], pred_cup[:row_num,:,:,:], gt_disc[:row_num,:,:,:], gt_cup[:row_num,:,:,:])
+        # compose = torch.cat((imgs[:row_num,:,:,:],pred_disc[:row_num,:,:,:], pred_cup[:row_num,:,:,:], gt_disc[:row_num,:,:,:], gt_cup[:row_num,:,:,:]),0)
+        compose = torch.cat((pred_disc[:row_num,:,:,:], pred_cup[:row_num,:,:,:], gt_disc[:row_num,:,:,:], gt_cup[:row_num,:,:,:]),0)
+        vutils.save_image(compose, fp = save_path, nrow = row_num, padding = 10)
+    else:
+        imgs = torchvision.transforms.Resize((h,w))(imgs)
+        if imgs.size(1) == 1:
+            imgs = imgs[:,0,:,:].unsqueeze(1).expand(b,3,h,w)
+        pred_masks = pred_masks[:,0,:,:].unsqueeze(1).expand(b,3,h,w)
+        gt_masks = gt_masks[:,0,:,:].unsqueeze(1).expand(b,3,h,w)
+        if points != None:
+            for i in range(b):
+                if args.thd:
+                    p = np.round(points.cpu()/args.roi_size * args.out_size).to(dtype = torch.int)
+                else:
+                    p = np.round(points.cpu()/args.image_size * args.out_size).to(dtype = torch.int)
+                # gt_masks[i,:,points[i,0]-5:points[i,0]+5,points[i,1]-5:points[i,1]+5] = torch.Tensor([255, 0, 0]).to(dtype = torch.float32, device = torch.device('cuda:' + str(dev)))
+                gt_masks[i,0,p[i,0]-5:p[i,0]+5,p[i,1]-5:p[i,1]+5] = 0.5
+                gt_masks[i,1,p[i,0]-5:p[i,0]+5,p[i,1]-5:p[i,1]+5] = 0.1
+                gt_masks[i,2,p[i,0]-5:p[i,0]+5,p[i,1]-5:p[i,1]+5] = 0.4
+        tup = (imgs[:row_num,:,:,:],pred_masks[:row_num,:,:,:], gt_masks[:row_num,:,:,:])
+        # compose = torch.cat((imgs[:row_num,:,:,:],pred_disc[:row_num,:,:,:], pred_cup[:row_num,:,:,:], gt_disc[:row_num,:,:,:], gt_cup[:row_num,:,:,:]),0)
+        compose = torch.cat(tup,0)
+        vutils.save_image(compose, fp = save_path, nrow = row_num, padding = 10)
+
+    return
+
+def eval_seg(pred,true_mask_p,threshold):
+    '''
+    threshold: a int or a tuple of int
+    masks: [b,2,h,w]
+    pred: [b,2,h,w]
+    '''
+    b, c, h, w = pred.size()
+    if c == 2:
+        iou_d, iou_c, disc_dice, cup_dice = 0,0,0,0
+        for th in threshold:
+
+            gt_vmask_p = (true_mask_p > th).float()
+            vpred = (pred > th).float()
+            vpred_cpu = vpred.cpu()
+            disc_pred = vpred_cpu[:,0,:,:].numpy().astype('int32')
+            cup_pred = vpred_cpu[:,1,:,:].numpy().astype('int32')
+
+            disc_mask = gt_vmask_p [:,0,:,:].squeeze(1).cpu().numpy().astype('int32')
+            cup_mask = gt_vmask_p [:, 1, :, :].squeeze(1).cpu().numpy().astype('int32')
+    
+            '''iou for numpy'''
+            iou_d += iou(disc_pred,disc_mask)
+            iou_c += iou(cup_pred,cup_mask)
+
+            '''dice for torch'''
+            disc_dice += dice_coeff(vpred[:,0,:,:], gt_vmask_p[:,0,:,:]).item()
+            cup_dice += dice_coeff(vpred[:,1,:,:], gt_vmask_p[:,1,:,:]).item()
+            
+        return iou_d / len(threshold), iou_c / len(threshold), disc_dice / len(threshold), cup_dice / len(threshold)
+    else:
+        eiou, edice = 0,0
+        for th in threshold:
+
+            gt_vmask_p = (true_mask_p > th).float()
+            vpred = (pred > th).float()
+            vpred_cpu = vpred.cpu()
+            disc_pred = vpred_cpu[:,0,:,:].numpy().astype('int32')
+
+            disc_mask = gt_vmask_p [:,0,:,:].squeeze(1).cpu().numpy().astype('int32')
+    
+            '''iou for numpy'''
+            eiou += iou(disc_pred,disc_mask)
+
+            '''dice for torch'''
+            edice += dice_coeff(vpred[:,0,:,:], gt_vmask_p[:,0,:,:]).item()
+            
+        return eiou / len(threshold), edice / len(threshold)
+
+def generate_click_prompt(img, msk, pt_label = 1):
+    # return: prompt, prompt mask
+    pt_list = []
+    msk_list = []
+    b, c, h, w, d = msk.size()
+    msk = msk[:,0,:,:,:]
+    for i in range(d):
+        pt_list_s = []
+        msk_list_s = []
+        for j in range(b):
+            msk_s = msk[j,:,:,i]
+            indices = torch.nonzero(msk_s)
+            if indices.size(0) == 0:
+                # generate a random array between [0-h, 0-h]:
+                random_index = torch.randint(0, h, (2,)).to(device = msk.device)
+                new_s = msk_s
+            else:
+                random_index = random.choice(indices)
+                label = msk_s[random_index[0], random_index[1]]
+                new_s = torch.zeros_like(msk_s)
+                # convert bool tensor to int
+                new_s = (msk_s == label).to(dtype = torch.float)
+                # new_s[msk_s == label] = 1
+            pt_list_s.append(random_index)
+            msk_list_s.append(new_s)
+        pts = torch.stack(pt_list_s, dim=0)
+        msks = torch.stack(msk_list_s, dim=0)
+        pt_list.append(pts)
+        msk_list.append(msks)
+    pt = torch.stack(pt_list, dim=-1)
+    msk = torch.stack(msk_list, dim=-1)
+
+    msk = msk.unsqueeze(1)
+
+    return img, pt, msk #[b, 2, d], [b, c, h, w, d]
